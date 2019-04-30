b13e92c40566071d8cfda425eec9bdb06f6f84e2
Vlad Rozhnev <rozhnev.vlad@gmail.com>
2019-05-01 02:39:42 +0300

    Create index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+/** @format */
+
+export namespace ColorManager {
+  export function setStatusBarColor(color: string, animated?: boolean, duration?: number): void;
+  export function setNavigationBarColor(color: string, animated?: boolean, duration?: number): void;
+  export function setRecentColor(color: string): void;
+}
